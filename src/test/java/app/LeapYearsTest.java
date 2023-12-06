4726dd7ad9e99e1486bd501a9f8f0ab17d1ef551
Nassim Errahoui <nerrahoui@ippon.fr>
2023-12-06 15:23:26 +0100

    Add JUnit

---
+++ src/test/java/app/LeapYearsTest.java
@@ -0,0 +1,11 @@
+package app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LeapYearsTest {
+    @Test
+    public void firstTest() {
+        Assertions.assertTrue(true);
+    }
+}
